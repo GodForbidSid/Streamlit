38a646d215cedfebefc7cabe0b85df96053f436a
Sanidhya Srivastava <147022548+GodForbidSid@users.noreply.github.com>
2025-07-18 08:37:56 +0530

    Delete Welcome.py

--- Welcome.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Welcome to Streamlit", layout="wide")
-st.title("Welcome to Streamlit!")
-name = st.text_input("Enter your name:")
-st.button("Submit", on_click=lambda: st.write("Hello,", name," welcome to the app!"))
-st.write("This is a simple Streamlit app to welcome users and collect their names.")
